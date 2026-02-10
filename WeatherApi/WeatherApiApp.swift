a3578dd84056122083d52eaa7d02c7b659421704
kcthedev <kc.thomas304@gmail.com>
2026-02-09 20:39:38 -0500

    Initial Commit

---
+++ WeatherApi/WeatherApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApiApp.swift
+//  WeatherApi
+//
+//  Created by KC Thomas on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherApiTests/WeatherApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApiTests.swift
+//  WeatherApiTests
+//
+//  Created by KC Thomas on 2/9/26.
+//
+
+import Testing
+@testable import WeatherApi
+
+struct WeatherApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
