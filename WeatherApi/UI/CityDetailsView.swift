2e84bef343c55495a864c45d33baa35125c0e2b6
kcthedev <kc.thomas304@gmail.com>
2026-02-14 23:11:03 -0500

    Add background image

--- WeatherApi/UI/CityDetailsView.swift
+++ WeatherApi/UI/CityDetailsView.swift
@@ -7,8 +7,47 @@ struct CityDetailsView: View {
         self.details = details
     }
     
+    var isBackgroundDark: Bool {
+        switch details.weather {
+        case "Clear": true
+        default: false
+        }
+    }
+    
     var body: some View {
-        Text("Hello world")
-            .navigationTitle(details.name)
+        VStack(alignment: .leading) {
+            Text(details.weather)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .font(.system(size: 48))
+            Text(details.weatherDescription)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            HStack {
+                Text("Tempurature")
+                Spacer()
+                Text("\(details.temperature) F")
+            }
+            Divider()
+            HStack {
+                Text("Feels Like")
+                Spacer()
+                Text("\(details.feelsLike) F")
+            }
+            Spacer()
+        }
+        .background(background)
+        .foregroundColor(isBackgroundDark ? .white : .black)
+        .padding()
+        .navigationTitle(details.name)
+    }
+    
+    @ViewBuilder
+    var background: some View {
+        let resource = switch details.weather {
+        case "Clear": "clear"
+        default: "clouds"
+        }
+        Image(resource)
+            .ignoresSafeArea()
     }
 }
